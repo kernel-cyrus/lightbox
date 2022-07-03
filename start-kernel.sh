8677a593bd544d5e6d2810f0582bdca2857f37f2
archman <archman@126.com>
2022-07-03 12:42:39 +0800

    Update start-kernel.sh for both linux and android, add ddd support

--- start-kernel.sh
+++ start-kernel.sh
@@ -1,5 +1,6 @@
-KERNEL="../codebase/android-13/kernel-5.15/"
-INITRD="./prebuilts/rootfs/initrd_aarch64.cpio.gz"
+KERNEL=""
+INITRD=$(realpath ./prebuilts/rootfs/initrd_aarch64.cpio.gz)
+DEBUGGER="gdb"
 
 for i in "$@"; do
     case $i in
@@ -7,8 +8,12 @@ for i in "$@"; do
             KERNEL="${i#*=}"
             shift # past argument=value
             ;;
-         -initrd=*|--initrd=*)
+        -initrd=*|--initrd=*)
             INITRD="${i#*=}"
+            shift # past argument=value
+            ;;
+        -with=*|--with=*)
+            DEBUGGER="${i#*=}"
             shift # past argument=value
             ;;
         *)
@@ -18,68 +23,89 @@ for i in "$@"; do
     esac
 done
 
-KERNEL_VERSION=android13-5.15
+if [ -z "$KERNEL" ]; then
+    echo "Please pass the kernel path: --kernel=<path>"
+    return
+fi
 
-KERNEL_PATH=$(cd ${KERNEL}; pwd)
+if [ ! -d "$KERNEL" ]; then
+    echo "ERROR: Directory not found: --kernel=<path>"
+    return
+fi
 
-KERNEL_IMAGE=$KERNEL_PATH/out/$KERNEL_VERSION/dist/Image
+if [ -d "$KERNEL/common" ]; then
+    KERNEL_TYPE=android
+else
+    KERNEL_TYPE=linux
+fi
 
-KERNEL_VMLINUX=$KERNEL_PATH/out/$KERNEL_VERSION/common/vmlinux
+KERNEL_DIR=$(cd ${KERNEL}; pwd)
 
-GDB_SCRIPTS=$KERNEL_PATH/out/$KERNEL_VERSION/common/scripts/gdb/vmlinux-gdb.py
+if [ $KERNEL_TYPE == "android" ]; then
+    KERNEL_OUT=${KERNEL_DIR}/out/$(ls ${KERNEL_DIR}/out/)
+    KERNEL_SOURCE=${KERNEL_DIR}/common
+    KERNEL_IMAGE=${KERNEL_OUT}/dist/Image
+    KERNEL_VMLINUX=${KERNEL_OUT}/common/vmlinux
+    KERNEL_SCRIPTS=${KERNEL_OUT}/common/vmlinux-gdb.py
+    RELOC_SECTIONS="-s .head.text 0x40200000 -s .text 0x40210000 -s .init.text 0x42870000"
+elif [ $KERNEL_TYPE == "linux" ]; then
+    KERNEL_OUT=${KERNEL_DIR}
+    KERNEL_SOURCE=${KERNEL_DIR}
+    KERNEL_IMAGE=${KERNEL_OUT}/arch/arm64/boot/Image
+    KERNEL_VMLINUX=${KERNEL_OUT}/vmlinux
+    KERNEL_SCRIPTS=${KERNEL_OUT}/vmlinux-gdb.py
+    RELOC_SECTIONS="-s .head.text 0x40200000 -s .text 0x40210000 -s .init.text 0x418b0000"
+fi
 
-INITRD_PATH=${INITRD}
-
-rm -rf ./lookup
-
-mkdir ./lookup
-
-mkdir ./lookup/out
-
-mkdir ./lookup/out/$KERNEL_VERSION
-
-ln -sfn $KERNEL_PATH/common ./lookup/out/$KERNEL_VERSION/common
-
-ln -sfn $KERNEL_PATH/common ./lookup/common
-
-SOURCE_PATH=./lookup
-
+GDB_DIR=$(cd ./prebuilts/gdb; pwd)
+GDB_EXEC=${GDB_DIR}/usr/local/bin/aarch64-linux-gnu-gdb
+GDB_DATA=${GDB_DIR}/usr/local/share/gdb
+QEMU_EXEC=$(realpath ./prebuilts/qemu/usr/local/bin/qemu-system-aarch64)
+INITRD_PATH=$(realpath ${INITRD})
 DEBUG_PORT=$((10000 + $RANDOM % 10000))
 
-ln -sfn $KERNEL_PATH/common ./lookup/out/$KERNEL_VERSION/common
-
-ln -sfn $KERNEL_PATH/common ./lookup/common
-
-SOURCE_PATH=./lookup
-
-DEBUG_PORT=$((10000 + $RANDOM % 10000))
-
-gnome-terminal --                                           \
-./prebuilts/qemu/usr/local/bin/qemu-system-aarch64          \
- -machine virt                                              \
- -cpu cortex-a72                                            \
- -m 1024                                                    \
- -smp 4                                                     \
- -initrd $INITRD_PATH                                       \
- -kernel $KERNEL_IMAGE                                      \
- -serial stdio                                              \
- -display none                                              \
- -gdb tcp::$DEBUG_PORT                                      \
+gnome-terminal --               \
+${QEMU_EXEC}                    \
+ -machine virt                  \
+ -cpu cortex-a72                \
+ -m 1024                        \
+ -smp 4                         \
+ -initrd ${INITRD_PATH}         \
+ -kernel ${KERNEL_IMAGE}        \
+ -serial stdio                  \
+ -display none                  \
+ -gdb tcp::${DEBUG_PORT}        \
  -S -append nokaslr
 
-./prebuilts/gdb/usr/local/bin/aarch64-linux-gnu-gdb         \
- -directory $SOURCE_PATH                                    \
- -data-directory ./prebuilts/gdb/usr/local/share/gdb        \
- -ex "add-auto-load-safe-path $GDB_SCRIPTS"                 \
- -ex "set confirm off"                                      \
- -ex "file $KERNEL_VMLINUX"                                 \
- -ex "add-symbol-file $KERNEL_VMLINUX -s .head.text 0x40200000 -s .text 0x40210000 -s .init.text 0x42870000" \
- -ex "target remote localhost:$DEBUG_PORT"                  \
- -ex "b * 0x40200000 thread 1"                              \
- -ex "tui enable"                                           \
- -ex "continue"                                             \
- -quiet
+if [ $DEBUGGER == "gdb" ]; then
 
-rm -rf ./lookup
+    (cd ${KERNEL_SOURCE}; ${GDB_EXEC}                           \
+     -data-directory ${GDB_DATA}                                \
+     -ex "add-auto-load-safe-path ${KERNEL_SCRIPTS}"            \
+     -ex "set confirm off"                                      \
+     -ex "file ${KERNEL_VMLINUX}"                               \
+     -ex "add-symbol-file ${KERNEL_VMLINUX} ${RELOC_SECTIONS}"  \
+     -ex "target remote localhost:${DEBUG_PORT}"                \
+     -ex "b * 0x40200000 thread 1"                              \
+     -ex "tui enable"                                           \
+     -ex "continue"                                             \
+     -quiet)
+
+elif [ $DEBUGGER == "ddd" ]; then
+
+    DDD_SCRIPT=$(realpath ./ddd.script)
+
+    rm -f ${DDD_SCRIPT}
+    touch ${DDD_SCRIPT}
+    echo "add-auto-load-safe-path ${KERNEL_SCRIPTS}" >> ${DDD_SCRIPT}
+    echo "set confirm off" >> ${DDD_SCRIPT}
+    echo "file ${KERNEL_VMLINUX}" >> ${DDD_SCRIPT}
+    echo "add-symbol-file ${KERNEL_VMLINUX} ${RELOC_SECTIONS}" >> ${DDD_SCRIPT}
+    echo "target remote localhost:${DEBUG_PORT}" >> ${DDD_SCRIPT}
+    echo "b * 0x40200000 thread 1" >> ${DDD_SCRIPT}
+    echo "continue" >> ${DDD_SCRIPT}
+
+    (cd ${KERNEL_SOURCE}; ddd --debugger ${GDB_EXEC} --command ${DDD_SCRIPT})
+fi
 
 killall qemu-system-aarch64
