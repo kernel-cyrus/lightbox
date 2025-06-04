b72f472bcb0390e3427049df4645cc911cff8e71
Cyrus Huang <cyrus.kernel@gmail.com>
2025-06-04 11:04:58 +0800

    add qemu-aarch64-static fix chpasswd issue on x86

--- scripts/debian-rootfs/create-rootfs.sh
+++ scripts/debian-rootfs/create-rootfs.sh
@@ -1,3 +1,14 @@
+# Check dependency
+if ! command -v multistrap >/dev/null 2>&1; then
+    echo "Installing multistrap..."
+    sudo apt update && sudo apt install multistrap -y
+fi
+
+if ! command -v qemu-aarch64-static >/dev/null 2>&1; then
+    echo "Installing multistrap..."
+    sudo apt update && sudo apt install qemu-aarch64-static -y
+fi
+
 # Remove old rootfs directory
 sudo rm -rf rootfs
 
@@ -7,18 +18,15 @@ sudo multistrap -a arm64 -f debian-arm64.conf -d rootfs
 # Create init
 sudo ln -snf sbin/init rootfs/init
 
-# Set permission
-sudo chown "$(id -u):$(id -g)" -R rootfs
+# Setup arm64 excution environment
+sudo cp /usr/bin/qemu-aarch64-static ./rootfs/usr/bin/
+
+# Patch rootfs with overlay files
+sudo cp -r rootfs-overlays/. rootfs/
 
 # Set passowrd
 sudo chroot rootfs bash -c "echo root:root | chpasswd"
 
-# Setup network interface
-echo "auto eth0" >> rootfs/etc/network/interfaces
-echo "iface eth0 inet dhcp" >> rootfs/etc/network/interfaces
+# Change permission
+sudo chown "$(id -u):$(id -g)" -R rootfs
 
-# Setup DNS server
-touch rootfs/etc/resolv.conf
-echo "nameserver 223.5.5.5       # AliDNS" >> rootfs/etc/resolv.conf
-echo "nameserver 119.29.29.29    # DNSPod" >> rootfs/etc/resolv.conf
-echo "nameserver 114.114.114.114 # 114DNS" >> rootfs/etc/resolv.conf
